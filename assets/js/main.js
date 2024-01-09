75704faa00f6181224d4165f445d433f0b8bb259
GiacomoMilitello <g.militello95@gmail.com>
2024-01-09 16:26:45 +0100

    bold js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@ const ul = document.querySelector('ul.list');
             const element = document.createElement('li');
             if (i % 3 === 0 && i % 5 === 0) {
                 element.innerText = 'fizzbuzz';
-                element.classList.add('bg-danger', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center');
+                element.classList.add('bg-danger', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center', 'fw-bold');
             } else if (i % 3 === 0) {
                 element.innerText = 'fizz';
-                element.classList.add('bg-success', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center');
+                element.classList.add('bg-success', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center', 'fw-bold');
             } else if (i % 5 === 0) {
                 element.innerText = 'buzz';
-                element.classList.add('bg-warning', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center');
+                element.classList.add('bg-warning', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center', 'fw-bold');
             } else {
                 element.innerText = i;
-                element.classList.add('bg-primary', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center');
+                element.classList.add('bg-primary', 'ratio', 'd-flex', 'justify-content-center', 'align-items-center', 'fw-bold');
             }
             ul.appendChild(element);
         }
